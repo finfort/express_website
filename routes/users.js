2f0215b9bd8b0b75e04eda32946086e4b98ec951
finfort <anatoliy.ruchka@gmail.com>
2015-08-28 16:32:53 +0300

    found bug in validation

--- routes/users.js
+++ routes/users.js
@@ -6,27 +6,35 @@ var flash = require('connect-flash');
 
 var http = require('http');
 var inspect = require('util').inspect,
-    path = require('path'),
-    os = require('os'),
-    fs = require('fs');
+  path = require('path'),
+  os = require('os'),
+  fs = require('fs');
+// var bodyParser = require('body-parser');
+ 
+// var multer  = require('multer');
+// var upload = multer();
 
 var Busboy = require('busboy');
 
-var formidable = require('formidable')
-    
+// var formidable = require('formidable')
+
 
 var User = require('../models/user');
 
 
+//router.use(express.bodyParser());
+//router.use(express.urlencoded());
+//router.use(express.json());
+//var urlencodedParser = bodyParser.urlencoded({extended: false } );
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 
-router.get('/register', function(req,res,next){
-  
+router.get('/register', function (req, res, next) {
+
   res.render('register', {
     "title": "Register"
   });
@@ -34,179 +42,190 @@ router.get('/register', function(req,res,next){
 
 
 
-router.get('/login', function(req,res,next){
-  
+router.get('/login', function (req, res, next) {
+
   res.render('login', {
     "title": "Login"
   });
 });
+// var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 },
+router.post('/register', function (req, res, next) {
 
-router.post('/register', function(req,res,next){
-
-  //get from values
-  var name = req.body.name;
-  var email = req.body.email;
-  var username = req.body.username;
-  var password = req.body.password;
-  var password2 = req.body.password2;
-  
-  
-  // console.log(req.body);
-  //console.dir(req.files);
-  
   var profileImageName;
-  
-    var fstream;
-    var result = [];
-    var number_of_files = 1;
-    var counter = 0;
-    var busboy = new Busboy({ headers: req.headers });
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      counter++;
-      console.log("Uploading: " + filename);
-      console.log('File [' + file + '] field ' + fieldname + ' ...');
-      //Path where image will be uploaded
-      if (result.length > 0) {
-          var file_type = filename.substr(filename.length - 4);
-          filename = result[0].name + '_' + number_of_files + file_type;
-          number_of_files++;
+
+  var name;
+  var email;
+  var username;
+  var password;
+  var password2;
+
+  var fstream;
+  var result = [];
+  var number_of_files = 1;
+  var counter = 0;
+
+  email = req.params.email; 
+  password = req.body.password;
+
+  var busboy = new Busboy({ headers: req.headers });
+  busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+    counter++;
+    console.log("Uploading: " + filename);
+    console.log('File [' + file + '] field ' + fieldname + ' ...');
+    //Path where image will be uploaded
+    if (result.length > 0) {
+      var file_type = filename.substr(filename.length - 4);
+      filename = result[0].name + '_' + number_of_files + file_type;
+      number_of_files++;
+    }
+    fstream = fs.createWriteStream("./public/images/uploads/" + filename);
+    file.pipe(fstream);
+    fstream.on('close', function () {
+      counter--;
+      console.log("Upload Finished of " + filename);
+      result.push(filename);
+      profileImageName = result;
+      console.log("result ", result)
+      if (counter == 0) {
+        console.log("writing finished");
+        //res.sendStatus(200);
       }
-      fstream = fs.createWriteStream( "./public/images/uploads/" + filename);
-      file.pipe(fstream);
-        fstream.on('close', function() {
-            counter--;
-            console.log("Upload Finished of " + filename);
-            result.push(filename);
-            console.log("result ",result)
-            if(counter == 0){
-                console.log("writing finished");
-                //res.sendStatus(200);
-                
-            }
-        });
-      });
-      busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-        console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-      });
-      busboy.on('finish', function() {
-        console.log('Done parsing form!');
-        res.writeHead(303, { Connection: 'close', Location: '/' });
+    });
+  });
+  busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated) {
+    console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    //get from values
+    if (fieldname == "name")
+      name = val;
+    if (fieldname == "email")
+      email = val;
+    if (fieldname == "username")
+      username = val;
+    if (fieldname == "password")
+      password = val;
+    if (fieldname == "password2")
+      password2 = val;
+      
+            
+  });
+  busboy.on('finish', function () {
+    console.log('Done parsing form!');
+    //res.writeHead(303, { Connection: 'close', Location: '/' });
+    //res.end();
+    
+      
+  });
+
+  req.pipe(busboy);// carefull closing busboy
+    
+  if (!profileImageName)
+    profileImageName = 'noimage.png';
+    
+  
+// 50 
+// 0997750769
+  // if (name && email && username && password && password2) {
+     setTimeout(function(){ 
+
+        console.log("timeout");
         
-        res.end();
+        if(name && email && username && password && password2){
+             //form validation
+    req.check('name', 'Name field is required').notEmpty();
+    req.check('email', 'Email field is required').notEmpty();
+    req.check('email', 'Email not valid').isEmail();
+    req.check('username', 'Username field is required').notEmpty();
+    req.check('password', 'Password field is required').notEmpty();
+    req.check('password2', 'Passwords do not match').equals(password);
+  
+  
+    // Check for errors
+  
+    var errors = req.validationErrors();
+
+    if (errors) {
+      res.render('register', {
+        errors: errors,
+        name: name,
+        email: email,
+        username: username,
+        password: password,
+        password2: password2
       });
-    // }
-    req.pipe(busboy);// carefull closing busboy
-    
-    if(!profileImageName)
-      profileImageName = 'noimage.png';
-    
-  // check for image
-  // if(req.files.profileimage){
-  //   console.log('uploading file....')
-  //   //file info
-  //   var profileImageOriginalName = req.files.profileimage.originalname;
-  //   var profileImageName         = req.files.profileimage.name;
-  //   var profileImageMime         = req.files.profileimage.mime;
-  //   var profileImagePath         = req.files.profileimage.path;
-  //   var profileImageExtension    = req.files.profileimage.extension;
-  //   var profileImageSize         = req.files.profileimage.size;
-    
-    
-  // }else{
-  //   // set a default Image
-  // }
-  
-  
-  // //form validation
-  // req.checkBody('name','Name field is required').notEmpty();
-  // req.checkBody('email','Email field is required').notEmpty();
-  // req.checkBody('email','Email not valid').isEmail();
-  // req.checkBody('username','Username field is required').notEmpty();
-  // req.checkBody('password','Password field is required').notEmpty();
-  // req.checkBody('password2','Passwords do not match').equals(req.body.password);
-  
-  
-  // // Check for errors
-  
-  // var errors = req.validationErrors();
-  
-  // if(errors){
-  //   res.render('register', {
-  //     errors: errors,
-  //     name: name,
-  //     email: email,
-  //     username: username,
-  //     password: password,
-  //     password2: password2
-  //   });
-    
-  // }else{  
-  //   console.log('cool');
-  //   // create User if all Ok
-  //   var newUser = new User({
-  //     name: name,
-  //     email: email,
-  //     username: username,
-  //     password: password,
-  //     profileimage: profileImageName
-  //   });
+
+    } else {
+      console.log('cool');
+      // create User if all Ok
+      var newUser = new User({
+        name: name,
+        email: email,
+        username: username,
+        password: password,
+        profileimage: profileImageName
+      });
+
+      User.createUser(newUser, function (err, user) {
+        if (err) throw err;
+        console.log(user);
+      });
    
-  //  User.createUser(newUser, function(err, user){
-  //    if(err) throw err;
-  //    console.log(user);
-  //  });
-   
-   // Success Message
-   req.flash('success', "You are now regestered and may log in");
-   
-   
-   res.location('/');
-   res.redirect('/'); 
+      // Success Message
+      req.flash('success', "You are now regestered and may log in");
+
+
+      res.location('/');
+      res.redirect('/');
+
+    //}
+
+    }
+        }
     
-  //}
-  
+    }, 3000);  
+ 
+
 });
 
-passport.serializeUser(function(user,done){
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
 
-passport.deserializeUser(function(id,done){
-  User.getUserById(id, function(err, user){
+passport.deserializeUser(function (id, done) {
+  User.getUserById(id, function (err, user) {
     done(err, user);
   });
 });
 
 passport.use(new LocalStrategy(
-  function(username, password, done){
-    
-    User.getUserByUsername(username, function(err, user){
-      if(err) throw err;
-      if(!user){
+  function (username, password, done) {
+
+    User.getUserByUsername(username, function (err, user) {
+      if (err) throw err;
+      if (!user) {
         console.log("Unknown user");
-        return done(null, false,{message: "Uknown user"});
+        return done(null, false, { message: "Uknown user" });
       }
-       User.comparePassword(password, user.password, function(err, isMatch){
-         if(err) throw err;
-         if(isMatch){
-           return done(null,user);
-         }else{
-           console.log('Invalid Password');
-           return done(null, false, {message: 'Invalid Password'});
-         }
-       });
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          console.log('Invalid Password');
+          return done(null, false, { message: 'Invalid Password' });
+        }
+      });
     });
   }
-));
+  ));
 
-router.post('/login', passport.authenticate('local',{failureRedirect:'/users/login', failureFlash:"Invalid username or password"}),function(req,res){
+router.post('/login', passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: "Invalid username or password" }), function (req, res) {
   console.log("Authentication successful");
-  req.flash("success","You are logged in");
+  req.flash("success", "You are logged in");
   res.redirect('/');
 });
 
-router.get('/logout', function(req,res, next){
+router.get('/logout', function (req, res, next) {
   req.logout();
   req.flash("success", "You have logged out");
   res.redirect('/users/login');
